51c8c98b0b82039f2ca4f3fe3e0a9c82abc0a41d
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-10-20 22:42:39 +0200

    added login screen

--- build/header.php
+++ build/header.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Weekly Meal Planner</title>
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
--- src/php/index.php
+++ src/php/index.php
@@ -1,7 +1,33 @@
 <?php include "header.php"; ?>
 
 <header>
+    <div class="headerline green"></div>
+    <div class="headerline dark"></div>
     <h1>WMP</h1>
+    <h2>Weekly Meal Planner</h2>
 </header>
+
+<section id="login">
+    <form action="#answer" class="form clearfix" id="contact-form" method="post" autocomplete="off" novalidate>
+
+        <fieldset>
+        <label for="name"><i class="fa fa-user"></i></label>
+        <input type="text" name="name" id="name" placeholder="Username" />
+        </fieldset>
+        
+        <fieldset>
+        <label for="subject"><i class="fa fa-lock"></i></label>
+        <input type="text" name="subject" id="subject" placeholder="Password" />
+        </fieldset>
+
+        <input type="submit" value="Log in" id="submit"/>
+
+        <div id="answer"></div>
+            <div id="second-choice" class="clearfix">
+                <div class="register"><a href="#">Forgot details?</a></div>
+                <div class="register f-right"><a href="#">Register</a></div>
+            </div>
+    </form>
+</section>
 
 <?php include "footer.php"; ?>
